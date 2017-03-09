537cdd0d21e38d57dfdca93bef98ee2bd3c541b0
Jaeho01 <jjang3@ocdsb.ca>
2017-03-09 13:52:50 -0500

    Update sketch.js

--- p5/Waving Snowman/sketch.js
+++ p5/Waving Snowman/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  ellipse(200, 200, 80, 80);
+// bottom
+rect(0, 350, 399, 50);
+// bottom circle
+ellipse(200, 300, 150, 150);
+// middle circle
+ellipse(200, 200, 100, 100);
+// top circle
+ellipse(200, 120, 75, 75);
+// left arm
+line(160, 200, 50, 100);
+// right arm
+line(240, 200, 350, 100);
 }

8e7f4f16b6957ad6d76d1cf12bffe1629fb867e3
Jaeho01 <jjang3@ocdsb.ca>
2017-04-05 14:08:39 -0400

    Add files via upload

---
+++ p5/Messi's House/sketch.js
@@ -0,0 +1,69 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  var xPos = 60;
+var yPos = 210;
+var houseLength = 275;
+var houseHeight = 140;
+
+// background
+background(116, 171, 212);
+
+// field
+fill(0, 255, 0);
+arc(198, 396, 445, 140, 180, 360);
+
+// house
+fill(219, 182, 81);
+rect(xPos, yPos, houseLength, houseHeight);
+
+// roof
+fill(97, 39, 0);
+triangle(xPos, yPos, xPos+houseLength*1.0, yPos, xPos+houseLength*0.5, yPos-houseHeight*0.5);
+
+// frontDoor
+fill(29, 152, 196);
+rect(xPos+houseLength/2.1, yPos+houseHeight*1.0, houseLength*0.10, houseHeight*-0.50);
+fill(0, 0, 0);
+ellipse(xPos+houseLength*0.55, yPos+houseHeight*0.80, houseLength*0.02, houseLength*0.02);
+
+// windows
+fill(255, 255, 255);
+ellipse(xPos+houseLength*0.25, yPos+houseHeight*0.40, houseLength*0.20, houseHeight*0.40);
+ellipse(xPos+houseLength*0.75, yPos+houseHeight*0.40, houseLength*0.20, houseHeight*0.40);
+line(xPos+houseLength*0.25, yPos+houseHeight*0.20, xPos+houseLength*0.25, yPos+houseHeight*0.59);
+line(xPos+houseLength*0.15, yPos+houseHeight*0.40, xPos+houseLength*0.35, yPos+houseHeight*0.40);
+line(xPos+houseLength*0.65, yPos+houseHeight*0.40, xPos+houseLength*0.85, yPos+houseHeight*0.40);
+line(xPos+houseLength*0.75, yPos+houseHeight*0.2, xPos+houseLength*0.75, yPos+houseHeight*0.6);
+
+var xPos = 139;
+var yPos = 285;
+var houseLength = 120;
+var houseHeight = 65;
+
+
+// house
+fill(219, 182, 81);
+rect(xPos, yPos, houseLength, houseHeight);
+
+// roof
+fill(97, 39, 0);
+triangle(xPos, yPos, xPos+houseLength*1.0, yPos, xPos+houseLength*0.5, yPos-houseHeight*0.5);
+
+// frontDoor
+fill(29, 152, 196);
+rect(xPos+houseLength/2.1, yPos+houseHeight*1.0, houseLength*0.10, houseHeight*-0.50);
+fill(0, 0, 0);
+ellipse(xPos+houseLength*0.55, yPos+houseHeight*0.80, houseLength*0.02, houseLength*0.02);
+
+// windows
+fill(255, 255, 255);
+ellipse(xPos+houseLength*0.25, yPos+houseHeight*0.40, houseLength*0.20, houseHeight*0.40);
+ellipse(xPos+houseLength*0.75, yPos+houseHeight*0.40, houseLength*0.20, houseHeight*0.40);
+line(xPos+houseLength*0.25, yPos+houseHeight*0.20, xPos+houseLength*0.25, yPos+houseHeight*0.59);
+line(xPos+houseLength*0.15, yPos+houseHeight*0.40, xPos+houseLength*0.35, yPos+houseHeight*0.40);
+line(xPos+houseLength*0.65, yPos+houseHeight*0.40, xPos+houseLength*0.85, yPos+houseHeight*0.40);
+line(xPos+houseLength*0.75, yPos+houseHeight*0.2, xPos+houseLength*0.75, yPos+houseHeight*0.6);
+}

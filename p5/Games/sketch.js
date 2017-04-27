d28774e33b62f203c61a7bc1e89548faee5d7b95
Jaeho01 <jjang3@ocdsb.ca>
2017-04-26 20:21:53 -0400

    Update sketch.js

--- p5/Games/sketch.js
+++ p5/Games/sketch.js
@@ -2,10 +2,11 @@
 Jaeho Jang
 2017-4-13*/
 
-var posX = 100;
-var posY = 200;
-var speedX = 2;
-var speedY = 1;
+var posX = 400;
+var posY = 400;
+var ballLength = 100;
+var speedX = 5;
+var speedY = 5;
 
 function setup() {
 	createCanvas(400, 400);
@@ -14,9 +15,24 @@ function setup() {
 }
 
 function draw() {
-	strokeWeight(0.5);
-	fill(135,206,250);
-	ellipse(posX, posY, 20, 20);	
+	strokeWeight(1.5);
+	ellipse(posX, posY, ballLength, ballLength);
+	fill(0, 0, 0);
+	//pentagon
+	beginShape();
+	vertex(posX, posY - ballLength/4);
+	vertex(posX - ballLength/3.5, posY);
+	vertex(posX - ballLength/5.5, posY + ballLength/3.5);
+	vertex(posX + ballLength/5.5, posY + ballLength/3.5);
+	vertex(posX + ballLength/3.5, posY);
+	endShape(CLOSE);
+	//lines
+	line(posX, posY - ballLength/4, posX, posY - ballLength/2);
+	line(posX - ballLength/3.5, posY, posX - ballLength/2, posY - ballLength/9);
+	line(posX - ballLength/5.5, posY + ballLength/3.5, posX - ballLength/3.5, posY + ballLength/2.5);
+	line(posX + ballLength/5.5, posY + ballLength/3.5, posX + ballLength/3.5, posY + ballLength/2.5);
+	line(posX + ballLength/3.5, posY, posX + ballLength/2, posY - ballLength/9);	
+	
 	posY = posY + speedY + 1;
 	posX = posX + speedX + 1;
 	if (posX >= 400) {

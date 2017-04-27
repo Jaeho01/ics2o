f5a98d6c65b29d907572d11dea78e3e0068c5595
Jaeho01 <jjang3@ocdsb.ca>
2017-04-26 20:51:55 -0400

    Update sketch.js

--- p5/Games/sketch.js
+++ p5/Games/sketch.js
@@ -13,29 +13,33 @@ function setup() {
 }
 
 function draw() {
-	background(0, 104, 10);
-	strokeWeight(1.5);
-	fill(255, 255, 255);
-	ellipse(posX, posY, ballLength, ballLength);
-	fill(0, 0, 0);
+	background(0, 104, 10); //background
+	strokeWeight(1.5); 
+	fill(255, 255, 255); //colour of the ball (only for the circle)
+	ellipse(posX, posY, ballLength, ballLength); //circle part of the ball
+	fill(0, 0, 0); //colour for the lines and the pentagon on the ball
 	//pentagon
 	beginShape();
-	vertex(posX, posY - ballLength/4);
-	vertex(posX - ballLength/3.5, posY);
-	vertex(posX - ballLength/5.5, posY + ballLength/3.5);
-	vertex(posX + ballLength/5.5, posY + ballLength/3.5);
-	vertex(posX + ballLength/3.5, posY);
+	vertex(posX, posY - ballLength/4); //top point of the pentagon
+	vertex(posX - ballLength/3.5, posY); //left point of the pentagon
+	vertex(posX - ballLength/5.5, posY + ballLength/3.5); //bottom left point of the pentagon
+	vertex(posX + ballLength/5.5, posY + ballLength/3.5); //bottom right point of the pentagon
+	vertex(posX + ballLength/3.5, posY); //right point of the pentagon
 	endShape(CLOSE);
-	//lines
+	//lines on the ball
 	line(posX, posY - ballLength/4, posX, posY - ballLength/2);
 	line(posX - ballLength/3.5, posY, posX - ballLength/2, posY - ballLength/9);
 	line(posX - ballLength/5.5, posY + ballLength/3.5, posX - ballLength/3.5, posY + ballLength/2.5);
 	line(posX + ballLength/5.5, posY + ballLength/3.5, posX + ballLength/3.5, posY + ballLength/2.5);
 	line(posX + ballLength/3.5, posY, posX + ballLength/2, posY - ballLength/9);	
+	
 	speedY = speedY + 2;
 
 	posY = posY + speedY;
 	posX = posX + speedX;
+	
+	speedX = speedX - 1;
+	speedY = speedY - 1;
 	
 	if (posX >= 950) {
 		speedX = -speedX

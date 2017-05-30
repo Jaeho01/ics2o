ab850f1ba0860b8ad1e056b7e8e2b716ad53dc6b
Jaeho01 <jjang3@ocdsb.ca>
2017-05-30 12:46:55 -0400

    Update sketch.js

--- p5/Summative Game/sketch.js
+++ p5/Summative Game/sketch.js
@@ -1,8 +1,36 @@
+/*Summative Game
+  Jaeho Jang
+  2017-05-30 */
+
+var posX = 500;
+var posY = 500;
+var ballWidth = 50;
+var speedX = 5;
+var speedY = 5;
+
+
 function setup() {
   createCanvas(1000, 1000);
   
 }
 
 function draw() {
-  ellipse(500, 500, 50, 50);
+  ellipse(posX, posY, ballWidth, ballWidth);
+  posX = posX + speedX;
+  posY = posY + speedY;
+  if (posX > 1000) {
+      speedX = -speedX
+      }
+  if (posX < 0) {
+      speedX = -speedX
+    }
+  if (posY > 1000) {
+      speedY = -speedY
+    }
+  if (posY < 0) {
+      speedY = -speedY
+    }
+  
+  
+  
 }

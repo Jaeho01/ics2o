5c7af841dc8b2e8f4dfd3f295cc9ddb75de9df2a
Jaeho01 <jjang3@ocdsb.ca>
2017-06-02 12:44:50 -0400

    Update sketch.js

--- p5/Summative Game/sketch.js
+++ p5/Summative Game/sketch.js
@@ -2,35 +2,61 @@
   Jaeho Jang
   2017-05-30 */
 
-var posX = 500;
-var posY = 500;
+var posX = 250;
+var posY = 250;
 var ballWidth = 50;
-var speedX = 5;
+var speedX = 2;
 var speedY = 5;
+var brickX = 200;
+var brickY = 475;
+var speedBrick = 10;
+
 
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(500, 500);
   
 }
 
 function draw() {
+  background(152,251,152);
   ellipse(posX, posY, ballWidth, ballWidth);
   posX = posX + speedX;
   posY = posY + speedY;
-  if (posX > 1000) {
+  if (posX > 475) {
       speedX = -speedX
       }
-  if (posX < 0) {
+  if (posX < 25) {
       speedX = -speedX
     }
-  if (posY > 1000) {
+
+  if (posY < 25) {
       speedY = -speedY
     }
-  if (posY < 0) {
-      speedY = -speedY
-    }
   
+  if (keyIsDown(RIGHT_ARROW)) { 
+		brickX += speedBrick;
+		if (brickX >= 400) {
+			brickX = 400;
+		  } 
+	}
+  if (keyIsDown(LEFT_ARROW)) { 
+		brickX -= speedBrick;
+		if (brickX <= 0) {
+			brickX = 0;
+		  }
+		  
+	}
+  if ((posX >= brickX) && (posX < brickX+100) && (posY >= brickY-25)) {
+	  speedX = -speedX;
+	  speedY = -speedY;
+	  posY > brickY-25;
+  }
   
+  brickB();
   
+}
+
+function brickB() {
+	rect(brickX, brickY, 100, 25);
 }

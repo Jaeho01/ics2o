2756d94616f613e574faa379353c2b6dc551eb95
Jaeho01 <jjang3@ocdsb.ca>
2017-06-03 19:09:10 -0400

    Update sketch.js

--- p5/Summative Game/sketch.js
+++ p5/Summative Game/sketch.js
@@ -50,14 +50,14 @@ function draw() {
 		posY > brickY-25;
 		}
 	brickB();
-	/*bricks();
 	bricks();
 	bricks();
 	bricks();
 	bricks();
 	bricks();
 	bricks();
-	bricks();*/
+	bricks();
+	bricks();
 	if (posY >= 525) {
 		fill(255, 0, 0);
 		rect (0, 0, 500, 500);
@@ -65,13 +65,13 @@ function draw() {
 		text("YOU FAILED", 200, 150);
 		fill(50, 205, 50);
 		rect(125, 350, 250, 50);
-			if(mouseX >= 125 && mouseX <= 375 && mouseY >= 350 && mouseY <= 400) {
+			/*if(mouseX >= 125 && mouseX <= 375 && mouseY >= 350 && mouseY <= 400) {
 				fill(0, 255, 127);
 				mouseIsPressed() {
 					
 					}
 				}
-		}
+		}*/
 	
 
 	
@@ -81,6 +81,6 @@ function draw() {
 function brickB() {
 	rect(brickX, brickY, 100, 25);
 }
-/*function bricks() {
+function bricks() {
 	rect(random(0, 500), random(0, 400), 75, 25);
-}*/
+}

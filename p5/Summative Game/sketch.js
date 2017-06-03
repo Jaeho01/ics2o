840e9ade580882b2647b96d83ce9f16363fa65ac
Jaeho01 <jjang3@ocdsb.ca>
2017-06-03 19:06:17 -0400

    Update sketch.js

--- p5/Summative Game/sketch.js
+++ p5/Summative Game/sketch.js
@@ -50,6 +50,14 @@ function draw() {
 		posY > brickY-25;
 		}
 	brickB();
+	bricks();
+	bricks();
+	bricks();
+	bricks();
+	bricks();
+	bricks();
+	bricks();
+	bricks();
 	if (posY >= 525) {
 		fill(255, 0, 0);
 		rect (0, 0, 500, 500);
@@ -72,4 +80,7 @@ function draw() {
 
 function brickB() {
 	rect(brickX, brickY, 100, 25);
+}
+function bricks() {
+	rect(random(0, 500), random(0, 400), 75, 25);
 }

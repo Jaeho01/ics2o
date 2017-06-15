5f641f6619f110719a9dcb7d638fb81c1d03a2a5
Jaeho01 <jjang3@ocdsb.ca>
2017-06-15 13:48:16 -0400

    Create sketch.js

--- p5/Summative Game/sketch.js
+++ p5/Summative Game/sketch.js
@@ -74,7 +74,7 @@ function howTo() {
 	fill(0, 0, 128); //make the colour of the rectangle blue
 	textSize(30);
 	text("How to Play the Game", 100, 100);
-	rect(125, 350, 250, 50); //draw the rectangle
+	rect(125, 400, 250, 50); //draw the rectangle
 	fill(255, 255, 255); //set the text colour to white
 	textSize(30);
 	text("Back", 157.5, 385); //display "Back" in the rectangle that had been drawn 
@@ -82,10 +82,10 @@ function howTo() {
 	strokeWeight(5);
 	fill(0, 0, 0);
 	line(250, 350, 250, 400);
-	if(mouseIsPressed && (mouseX >= 125) && (mouseX <= 250) && (mouseY >= 350) && (mouseY <= 400)) { //when the back box is clicked, change the status to 0
+	if(mouseIsPressed && (mouseX >= 125) && (mouseX <= 250) && (mouseY >= 400) && (mouseY <= 450)) { //when the back box is clicked, change the status to 0
 		status = 0;
 	}
-	if(mouseIsPressed && (mouseX >= 250) && (mouseX <= 375) && (mouseY >= 350) && (mouseY <= 400)) { //when the back box is clicked, change the status to 0
+	if(mouseIsPressed && (mouseX >= 250) && (mouseX <= 375) && (mouseY >= 400) && (mouseY <= 450)) { //when the back box is clicked, change the status to 0
 		status = 1;
 	}
 }

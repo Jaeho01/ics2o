05321a42734d020c7c4158b63d249c327fbaa018
Jaeho01 <jjang3@ocdsb.ca>
2017-06-16 12:01:31 -0400

    Update sketch.js

--- p5/Summative Game/sketch.js
+++ p5/Summative Game/sketch.js
@@ -80,7 +80,7 @@ function howTo() {
 	text("The goal of the game is to get the highest score by keeping the balls in the screen", 20, 150);
 	text("You will be able to control the brick at the bottom with your left-arrow key and right-arrow key", 20, 200);
 	text("The brick at the bottom will bounce the balls back to the screen", 20, 250);
-	text("The ball will bounce of the bricks on the screen sometimes", 20, 300);
+	text("The ball will bounce off the bricks on the screen, sometimes", 20, 300);
 	text("If one of the ball hits the bottom side, the game's over", 20, 350);
 	fill(255, 255, 255); //set the text colour to white
 	textSize(30);
